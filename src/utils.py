7a008020e1a449d983936d502f3380107620f72d
sairam751 <sairam751348@gmail.com>
2024-08-03 11:34:47 +0530

    Predictionpipeline and Flask UI

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 
 import numpy as np
 import pandas as pd
+import pickle
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
@@ -52,3 +53,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
  
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
